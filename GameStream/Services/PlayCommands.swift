63c37e391a68167bfe8524e57956bd94a810eb10
SIKEØ <157708940+ITZproVenom@users.noreply.github.com>
2026-09-14 00:42:55 +0530

    iOS: Play now reuses openGame tracking then launches

--- GameStream/Services/PlayCommands.swift
+++ GameStream/Services/PlayCommands.swift
@@ -3,11 +3,10 @@ import Foundation
 extension SessionStore {
     /// Jump straight into Xbox Cloud launch instead of the catalog page.
     func playGame(_ game: TrackedGame) {
-        guard let url = game.launchURL else { return }
-        webURL = url
-        isStreaming = false
-        requestedTab = .library
-        noteGame(id: game.id, slug: game.slug, title: game.title, markRecent: true)
+        openGame(game)
+        if let url = game.launchURL {
+            webURL = url
+        }
     }
 
     func playCurrent() {
