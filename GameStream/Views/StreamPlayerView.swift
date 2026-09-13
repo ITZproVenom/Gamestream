c9d9c4e92be127a1206a8a86e4e5ad92fa8c0cd4
ITZproVenom <157708940+ITZproVenom@users.noreply.github.com>
2026-09-13 17:57:20 +0530

    Update StreamPlayerView.swift

--- GameStream/Views/StreamPlayerView.swift
+++ GameStream/Views/StreamPlayerView.swift
@@ -10,32 +10,74 @@ struct StreamPlayerView: View {
 }
 
 struct XboxCloudWebView: UIViewRepresentable {
-    func makeCoordinator() -> Coordinator { Coordinator() }
+    let url: URL
+
+    init(url: URL = URL(string: "https://www.xbox.com/play")!) {
+        self.url = url
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
 
     func makeUIView(context: Context) -> WKWebView {
         let config = WKWebViewConfiguration()
         config.allowsInlineMediaPlayback = true
-        let webView = WKWebView(frame: .zero, configuration: config)
+
+        let webView = WKWebView(
+            frame: .zero,
+            configuration: config
+        )
+
         webView.navigationDelegate = context.coordinator
-        webView.load(URLRequest(url: URL(string: "https://www.xbox.com/play")!))
+
+        webView.load(
+            URLRequest(url: url)
+        )
+
         return webView
     }
 
-    func updateUIView(_ uiView: WKWebView, context: Context) {}
+    func updateUIView(
+        _ uiView: WKWebView,
+        context: Context
+    ) {}
 
     class Coordinator: NSObject, WKNavigationDelegate {
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            NotificationCenter.default.post(name: .webViewLoadingChanged, object: true)
+        func webView(
+            _ webView: WKWebView,
+            didStartProvisionalNavigation navigation: WKNavigation!
+        ) {
+            NotificationCenter.default.post(
+                name: .webViewLoadingChanged,
+                object: true
+            )
         }
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            NotificationCenter.default.post(name: .webViewLoadingChanged, object: false)
+
+        func webView(
+            _ webView: WKWebView,
+            didFinish navigation: WKNavigation!
+        ) {
+            NotificationCenter.default.post(
+                name: .webViewLoadingChanged,
+                object: false
+            )
         }
-        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-            NotificationCenter.default.post(name: .webViewLoadingChanged, object: false)
+
+        func webView(
+            _ webView: WKWebView,
+            didFail navigation: WKNavigation!,
+            withError error: Error
+        ) {
+            NotificationCenter.default.post(
+                name: .webViewLoadingChanged,
+                object: false
+            )
         }
     }
 }
 
 extension Notification.Name {
-    static let webViewLoadingChanged = Notification.Name("webViewLoadingChanged")
+    static let webViewLoadingChanged =
+        Notification.Name("webViewLoadingChanged")
 }
